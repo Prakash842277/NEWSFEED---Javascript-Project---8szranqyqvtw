a92d3fe392e9529a5318f8f1f2064703a081e6a9
Prakash Chandra <prakash140chandra@gmail.com>
2023-05-18 18:34:44 +0530

    Update App.js

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ const handleSavedNews = (savedItem) => {
   alert("News saved")
 }
 
-const getNews = (category = "Science") => {
+const getNews = (category = "science") => {
   newsContainer.innerHTML = "";
   fetch(`https://inshorts.deta.dev/news?category=${category}`)
     .then((response) => response.json())
